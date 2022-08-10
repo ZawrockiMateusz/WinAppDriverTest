71a58a9d3e54da593b99f90fd623471f16bd5f2a
Mateusz Zawrocki <mateusz.zawrocki@morlab.onmicrosoft.com>
2022-08-10 14:22:46 +0200

    Add project files.

---
+++ WinAppDriverTest/MozartSession.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WinAppDriverTest
+{
+    public class MozartSession
+    {
+        protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string MozartAppId = @"C:\Program Files (x86)\MozartIns\Mozart.exe";
+
+        protected static WindowsDriver<WindowsElement> session;
+        
+        public static void Setup(TestContext context)
+        {
+            // Launch a new instance of Mozart application
+            if (session == null)
+            {
+                // Create a new session to launch Mozart application
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", MozartAppId);
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+                Assert.IsNotNull(session);
+                Assert.IsNotNull(session.SessionId);
+            }
+
+        }
+        public static void TearDown()
+        {
+            // Close the application and delete the session
+            if (session != null)
+            {
+                session.Close();
+                session.Quit();
+                session = null;
+            }
+        }
+    }
+
+}
---
+++ WinAppDriverTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace WinAppDriverTest
+{
+    [TestClass]
+    public class UnitTest1 : MozartSession
+    {
+		[TestMethod]
+		public void ChooseDatabase()
+		{
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+            // Choose DataBase
+            session.FindElementByName("MozartWinAppDriver").Click();
+            session.FindElementByName("Ok").Click();
+            Thread.Sleep(2000);
+
+            session.SwitchTo().Window(session.WindowHandles.Last());
+            session.FindElementByName("Operator :").SendKeys(Keys.ArrowDown);
+            session.FindElementByName("PasswordTextEdit").Click();
+            session.FindElementByName("PasswordTextEdit").SendKeys("");
+            session.FindElementByName("Zaloguj").Click();
+            Thread.Sleep(2000);
+
+            session.SwitchTo().Window(session.WindowHandles.Last());
+            session.FindElementByAccessibilityId("cmdOk").Click();
+            Thread.Sleep(1000);
+
+            session.SwitchTo().Window(session.WindowHandles.Last());
+            session.FindElementByName("Zlecenia").Click();
+            Thread.Sleep(2000);
+            Assert.IsNotNull(session.FindElementByAccessibilityId("Zlecenia"));
+            Thread.Sleep(2000);
+        }
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+    }
+}
